9b5b460e9a757b6ef32cdc8a345ba6fe5fec30d6
kadammayuresh008 <kadam.ms@somaiya.edu>
2020-09-17 17:23:11 +0530

    product details with image file upload and mutiple input

--- Praticeprogram/project/home2.php
+++ Praticeprogram/project/home2.php
@@ -1,3 +1,45 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+  }
+
+  if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+  }
+
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+  $url='uploads/'.$_FILES["fileToUpload"]["name"];
+}
+?>
 <html>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -44,36 +86,47 @@ header('Location: http://localhost/Praticeprogram/project/productdetails.php');
 <div class="col-sm-10">
 <h2>Product details</h2>
 <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="image.jpeg" alt="Card image cap">
+<?php echo "<img src='{$url}' alt='Image not found.' style='width:280px; height:200px;'>"; ?>
   <div class="card-body">
   <?php
-if(!isset($_COOKIE['Mobile'])) {
+if(!isset($_POST['type'])) {
   echo "<h5>Product type</h5>";
-  echo $_COOKIE['Mobile'];
+  echo $_POST['type'];
   echo "<br/>";
 } else {
   echo "<h5>Product type</h5>";
-  echo  $_COOKIE['Mobile'];
+  echo  $_POST['type'];
   echo "<br/>";
 }
-if(!isset($_COOKIE['Mobilename'])) {
+if(!isset($_POST['Name'])) {
   echo "<h5>Product name</h5>";
-  echo  $_COOKIE['Mobilename'];
+  echo  $_POST['Name'];
   echo "<br/>";
 } else {
   echo "<h5>Product name</h5>";
-  echo  $_COOKIE['Mobilename'] ;
+  echo  $_POST['Name'] ;
   echo "<br/>";
 }
-if(!isset($_COOKIE['color'])) {
+if(!isset($_POST['color'])) {
   echo "<h5>Product color</h5>";
-    echo  $_COOKIE['color'];
+    echo  $_POST['color'];
     echo "<br/>";
   } else {
     echo "<h5>Product color</h5>";
-    echo $_COOKIE['color'];
+    echo $_POST['color'];
     echo "<br/>";
   }
+  if(!isset($_POST['vehicle'])) {
+    echo "<h5>Device reqired</h5>";
+    foreach($_POST['vehicle'] as $value){
+      echo "$value <br>";
+    }
+    } else {
+      echo "<h5>Device reqired</h5>";
+      foreach($_POST['vehicle'] as $value){
+        echo "$value <br>";
+      }
+    }
 ?>
 </div>
 </div>
--- Praticeprogram/project/productdetails.php
+++ Praticeprogram/project/productdetails.php
@@ -1,23 +1,11 @@
-<?php
-$name="color";
-$value="black";
-setcookie($name,$value,time()+(86400),"/");
-$type_name="Mobile";
-$type_value="Android";
-setcookie($type_name,$type_value,time()+(86400),"/");
-$name_name="Mobilename";
-$name_value="Redmi8A";
-setcookie($name_name,$name_value,time()+(86400),"/");
-?>
 <html>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
+integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <style>
 body{
     background: linear-gradient(to bottom, #33ccff 0%, #3333cc 100%);
- height:100%;
- width:100%;
 }
 input{
 border-radius:20px;
@@ -47,40 +35,16 @@ if(isset($_POST['product'])=="data")
 header('Location: http://localhost/Praticeprogram/project/productdetails.php');
 }
 ?>
-  <form class="form-inline" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"  method="post">
+  <form class="form-inline" action="home2.php"  method="post" enctype="multipart/form-data">
   <button name="product" value="data">Add</button>
     <button name="light" value="black">Logout</button>
   </form>
 </nav>
-<?php
-if(isset($_POST['Name']))
-{
-    $name_name="Mobilename";
-    $name_value=$_POST['Name'];
-    setcookie($name_name,$name_value,time()+(86400),"/");
-}
-if(isset($_POST['type']))
-{
-    $type_name="Mobile";
-    $type_value=$_POST['type'];
-    setcookie($type_name,$type_value,time()+(86400),"/");
-}
-if(isset($_POST['color']))
-{
-    $type_name="color";
-    $type_value=$_POST['color'];
-    setcookie($type_name,$type_value,time()+(86400),"/");
-}
-if(isset($_POST['Name']) and isset($_POST['type']) and isset($_POST['color']))
-{
-    header('Location: http://localhost/Praticeprogram/project/home2.php?');
-}
-?>
 <div align="center">
 <br>
 <h2>Upload Product</h2>
 <br>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="Post">
+<form action="home2.php" method="Post" enctype="multipart/form-data">
 <label><b>Product type</b></label>
 <input type="text" placeholder="Enter Type" name="type">
 <br>
@@ -93,7 +57,18 @@ if(isset($_POST['Name']) and isset($_POST['type']) and isset($_POST['color']))
 <input type="text" placeholder="Enter Color" name="color">
 <br>
 <br>
-<button type="Submit" name="light" value="black"><b>Upload</b></button>
+<label><b>The type of device required to play</b></label>
+<br>
+<br>
+<input type="checkbox" id="vehicle1" name="vehicle[]" value="Phone"><b>Phone</b></input>
+  <input type="checkbox" id="vehicle2" name="vehicle[]" value="Pc"><b>Pc</b></input>
+  <input type="checkbox" id="vehicle3" name="vehicle[]" value="Laptop"><b>Laptop</b></input>
+  <br><br>
+  <b>Select image to upload:</b>
+  <br>
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <br>
+  <input type="submit" value="Upload Image" name="submit">
 </form>
 <br>
 <div>
